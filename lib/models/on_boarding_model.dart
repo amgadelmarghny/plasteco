d19e8f721aa088615bd1db0a4e3200299b0d0354
amgadelmarghny <amgadsaad22@gmail.com>
2024-02-28 05:34:05 +0200

    create app cubit and create onboarding cubit

---
+++ lib/models/on_boarding_model.dart
@@ -0,0 +1,13 @@
+class OnBoardingModel {
+  final String image;
+  final String firstTitle;
+  final String secondTitle;
+  final String? therdTitle;
+
+  OnBoardingModel({
+    required this.image,
+    required this.firstTitle,
+    required this.secondTitle,
+    this.therdTitle,
+  });
+}
--- lib/modules/onboarding/onboarding_view.dart
+++ lib/modules/onboarding/onboarding_view.dart
@@ -2,9 +2,21 @@ import 'package:flutter/material.dart';
 
 class OnBoardingView extends StatelessWidget {
   const OnBoardingView({super.key});
-static const routeName = '/onboarding';
+  static const routeName = '/onboarding';
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: PageView.builder(
+              itemBuilder: (context, index) {
+                return null;
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
